ee4cb32b0fdc5db080fc9f1c5f8719858a619dd2
antonio <antonio.fr.martinezc@hotmail.com>
2024-08-22 20:51:21 -0400

    added links to desktop nav. implemented links visibility based on authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import saamLogo from "../assets/saam_logo.svg";
 import MobileMenu from "./MobileMenu";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 export default function Header() {
+	const { currentUser } = useAuth();
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<header className="text-light p-4 flex justify-between items-center">
@@ -13,13 +15,37 @@ export default function Header() {
 			>
 				menu
 			</span>
-			<nav className="hidden sm:flex gap-4">
-				<Link to="/auth" className="btn btn_lightdark">
-					Login
-				</Link>
-				<Link to="/auth/signup" className="btn btn_accent">
-					Create Account
-				</Link>
+			<nav className="hidden sm:flex gap-4 w-full justify-between">
+				<ul className="flex  gap-4 justify-self-center mx-auto ">
+					<li className="hover:text-accent transition duration-300 ease-in-out roboto-bold">
+						<Link to="/" className="flex py-2 gap-1 mb-1">
+							<span>Feed</span>
+						</Link>
+					</li>
+					{currentUser && (
+						<li className=" hover:text-accent transition duration-300 ease-in-out roboto-bold">
+							<Link to="/account" className="flex py-2 gap-1 mb-1">
+								<span>Profile</span>
+							</Link>
+						</li>
+					)}
+				</ul>
+				<div className="flex gap-4">
+					{currentUser ? (
+						<Link to="/auth/logout" className="btn btn_lightdark">
+							Logout
+						</Link>
+					) : (
+						<div className="flex  gap-2">
+							<Link to="/auth" className="btn btn_lightdark">
+								Login
+							</Link>
+							<Link to="/auth/signup" className="btn btn_accent">
+								Create Account
+							</Link>
+						</div>
+					)}
+				</div>
 			</nav>
 
 			<MobileMenu isOpen={isOpen} onSetIsOpen={setIsOpen} />
